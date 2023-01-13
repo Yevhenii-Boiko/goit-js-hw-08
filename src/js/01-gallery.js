8cc0aee4eb2009b6a1c95d3fef1ae920c5ef1d5f
Yevhenii <boiko.yevgenii@gmail.com>
2023-01-13 13:51:45 +0200

    adds task-01 solution

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,8 +3,29 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const sum = 2 + 3;
+const imagesContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryMarkup(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div>
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"
+                >
+            </a>
+        </div>`;
+    })
+    .join('');
+}
 
-console.log(sum);
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
