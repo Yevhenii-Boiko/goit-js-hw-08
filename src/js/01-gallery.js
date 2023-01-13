67beae1a4985f467db23f257da43dbb55032f10b
Yevhenii <boiko.yevgenii@gmail.com>
2023-01-13 12:56:49 +0200

    new const

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const sum = 2 + 3;
 
+console.log(sum);
 console.log(galleryItems);
 console.log(SimpleLightbox);
