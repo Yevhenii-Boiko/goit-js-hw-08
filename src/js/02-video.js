2b64eb915c150ad33c3d0e8446b438db648e2330
Yevhenii <boiko.yevgenii@gmail.com>
2023-01-19 17:25:11 +0200

    adds task-02 solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,36 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-console.log(Player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_STORAGE = 'videoplayer-current-time';
+
+addEventListener.timeupdate;
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(seconds));
+  }, 1000)
+);
+
+const savedCurrentTime = JSON.parse(localStorage.getItem(KEY_STORAGE));
+player
+  .setCurrentTime(savedCurrentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+console.log(player);
+// console.log(Player);
