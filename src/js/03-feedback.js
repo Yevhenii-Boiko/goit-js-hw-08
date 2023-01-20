48fa2a69334260552c71cf6369f99d8017ee849f
Yevhenii <boiko.yevgenii@gmail.com>
2023-01-20 17:38:07 +0200

    adds task-3 solution

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const KEY_STORAGE = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+  btn: document.querySelector('.feedback-form button'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+formInfo();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log({ email: refs.email.value, message: refs.message.value });
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY_STORAGE);
+}
+
+function onFormInput() {
+  const formData = JSON.stringify({
+    email: refs.email.value,
+    message: refs.message.value,
+  });
+
+  localStorage.setItem(KEY_STORAGE, formData);
+}
+
+function formInfo() {
+  const savedInfo = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (savedInfo) {
+    refs.email.value = savedInfo.email;
+    refs.message.value = savedInfo.message;
+  }
+}
